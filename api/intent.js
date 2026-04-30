8ef92d78f70a2d82db115c06e3de6435884d2c33
n-e-s-i-r-x <rakutadrakutan@gmail.com>
2026-05-01 03:48:48 +0800

    Update intent.js

--- api/intent.js
+++ api/intent.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
       headers: {
         'Authorization': `Bearer ${apiKey}`,
         'Content-Type': 'application/json',
-        'HTTP-Referer': 'https://mc-mod-builder.vercel.app',
+        'HTTP-Referer': 'https://0vmod.vercel.app',
         'X-Title': 'MC Intent'
       },
       body: JSON.stringify({
